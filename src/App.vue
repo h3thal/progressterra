e7366f905b56d7c8b8d424635438e5fc92005d82
Hethal <h3thal@yandex.ru>
2021-10-13 01:31:04 +0300

    git init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <Navbar/>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from '@/components/Navbar/Navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="nav">
+    <div class="nav__logo">
+      <div class="nav__logo-home">
+        <img src="@/assets/logo.png" alt="">
+        <a href="">metting</a>
+      </div>
+      <div class="menu__btn" :class="{ 'active' : this.isOpen  }" @click="this.isOpen = !this.isOpen">
+        <div class="menu__btn-block">
+          <span></span>
+        </div>
+      </div>
+    </div>
+    <div class="nav__links" :class="{ 'open' : this.isOpen }">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Feature</a></li>
+        <li><a href="">Pricing</a></li>
+        <li><a href="">Solution</a></li>
+        <li><a href="">Faq</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isOpen: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/__mixin.scss';
+@import '@/assets/scss/__extends.scss';
+// @import '@/assets/scss/__fonts.scss';
+a {
+  padding: 30px 0px;
+}
+.nav {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 60px;
+  @include md {
+    flex-direction: column;
+  }
+  &__logo {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    @include md {
+      width: 100%;
+      justify-content: space-evenly;
+    }
+    &-home {
+      display: flex;
+      align-items: center;
+      img {
+        height: 32px;
+      }
+      a {
+        text-decoration: none;
+        color: $navyBlue;
+        font-family: Inter;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 20px;
+        margin-left: 10px;
+      }
+    }
+    .menu__btn {
+      cursor: pointer;
+      height: 100%;
+      padding: 5px;
+      transition: .5s, background-color 0s linear;
+      display: none;
+      @include md {
+        display: flex;
+        align-items: center;
+      }
+      &.active {
+        span {
+          transform: rotate(90deg);
+          &::before {
+            top: 0px;
+            transform: rotate(45deg);
+          }
+          &::after {
+            top: 0px;
+            transform: rotate(-45deg);
+          }
+        }
+      }
+      &-block {
+        width: 30px;
+        height: 18px;
+        display: flex;
+        align-items: center;
+        position: relative;
+        span {
+          &, &::before, &::after {
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 2px;
+            background-color: #616161;
+            transition: .5s;
+          }
+          &::before {
+            content: '';
+            top: -8px;
+          }
+          &::after {
+            content: '';
+            top: 8px;
+          }
+        }
+      }
+    }
+  }
+  &__links {
+    height: 100%;
+    display: flex;
+    overflow-y: hidden;
+    transition: 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    @include md {
+      min-width: 100%;
+      width: 100%;
+      max-height: 0px;
+    }
+    &.open {
+      @include md {
+        max-height: 114px;
+      }
+    }
+    ul {
+      display: flex;
+      align-items: center;
+      flex-direction: row;
+      list-style: none;
+      padding: 0px;
+      margin: 0px;
+      height: 100%;
+      justify-content: space-between;
+      width: 300px;
+      @include md {
+        flex-direction: column;
+        width: 100%;
+      }
+      li {
+        a {
+          padding: 40px 0px;
+          text-decoration: none;
+          color: $navyBlue;
+          font-family: Inter;
+          font-style: normal;
+          font-weight: 500;
+          font-size: 18px;
+          line-height: 20px;
+          @include md {
+            padding: 10px 0px;
+          }
+        }
+        @include md {
+          padding: 10px 0px;
+        }
+      }
+    }
+  }
+}
+</style>
